3d37ae5c369b2d4287ec73407f269b5ff765435f
Maiana Sanjuan <maians01@nssrstudio03.csa.scot.nhs.uk>
2021-09-21 14:59:57 +0100

    initialized r demo script and uploaded sample data for practice

---
+++ r_demo_script.R
@@ -0,0 +1,12 @@
+### R Demo Script (Data Monitoring Induction Sept 2021)
+
+
+# Load R Libraries ----------------------------------------------------------
+library(readr)
+
+library(tidyverse)
+library(dplyr)
+
+# Load Data ---------------------------------------------------------------
+
+
